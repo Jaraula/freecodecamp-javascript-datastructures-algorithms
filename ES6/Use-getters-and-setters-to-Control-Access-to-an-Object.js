c7fed231dbfd038ddca1be3493b3c3eca26d7cb5
Md. Jamal Uddin <jamal.pb95@gmail.com>
2021-06-27 16:08:43 +0600

    es6 completed

--- ES6/Use-getters-and-setters-to-Control-Access-to-an-Object.js
+++ ES6/Use-getters-and-setters-to-Control-Access-to-an-Object.js
@@ -1,7 +1,19 @@
 class Thermostat {
-  construction(F) {
-    this._F = F;
+  constructor(F) {
+    this.F = F;
   }
 
-  get temperature() {}
+  get temperature() {
+    return (5 / 9) * (this.F - 32);
+  }
+
+  set temperature(C) {
+    this.F = (C * 9.0) / 5 + 32;
+  }
 }
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+console.log(temp);
+thermos.temperature = 26;
+temp = thermos.temperature;
